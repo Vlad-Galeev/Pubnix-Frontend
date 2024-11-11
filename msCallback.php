6c6575a33c15209081549abe4c46b275631c3aeb
Vlad <vlad.wpg@gmail.com>
2024-11-11 00:22:40 -0500

    Dev Commit spam

--- msCallback.php
+++ msCallback.php
@@ -8,20 +8,6 @@ use myPHPnotes\Microsoft\Handlers\Session;
 use myPHPnotes\Microsoft\Models\User;
 use myPHPnotes\Microsoft\Models\BetterUser;
 
-echo "TENNANT_ID ";
-echo Session::get("tenant_id");
-echo "<br>CLI_ID ";
-echo Session::get("client_id");
-echo "<br>CLI_SEC ";
-echo Session::get("client_secret");
-echo "<br>RED_URI ";
-echo Session::get("redirect_uri");
-echo "<br>SCOPE ";
-echo Session::get("scopes");
-echo "<br>CODE ";
-echo Session::get($_REQUEST['code']);
-echo "<br>STATE ";
-echo Session::get("state");
 $microsoft = new Auth(Session::get("tenant_id"),Session::get("client_id"),  Session::get("client_secret"), Session::get("redirect_uri"), Session::get("scopes"));
 $tokens = $microsoft->getToken($_REQUEST['code'], Session::get("state"));
 
@@ -29,7 +15,7 @@ $tokens = $microsoft->getToken($_REQUEST['code'], Session::get("state"));
 $microsoft->setAccessToken($tokens->access_token);
 
 $user = (new User); // User get pulled only if access token was generated for scope User.Read
-$betterUser = (new BetterUser);
+
 echo "<br>";
 echo $user->data->getGivenName();
 echo "<br>";
@@ -37,6 +23,7 @@ echo $user->data["_propDict"];
 echo "<br>";
 echo $user->data->getOnPremisesImmutableId();
 echo "<br>-------";
+$betterUser = (new BetterUser);
 echo $betterUser->data;
 // https://graph.microsoft.com/v1.0/me/memberOf
 

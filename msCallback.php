b2df4f8380ca01644a47df55b1cc3b10ab8b7f51
Vlad Galeev <vlad.wpg@gmail.com>
2024-11-11 11:38:36 -0500

    Update msCallback.php

--- msCallback.php
+++ msCallback.php
@@ -15,17 +15,20 @@ $microsoft->setAccessToken($tokens->access_token);
 
 $user = (new User); // User get pulled only if access token was generated for scope User.Read
 
-echo $user->data->getGivenName();
+//echo $user->data->getGivenName();
 $groups = $user->graph()->createRequest("get", "/me/memberOf")->execute()->getBody()["value"];
 $ids = array_column($groups, 'id');
-echo "<br>-------<br>";
-var_dump($groups);
-echo "<br>------<br>";
+//echo "<br>-------<br>";
+//var_dump($groups);
+//echo "<br>------<br>";
 var_dump($ids);
-echo "<br>------<br>";
-if (in_array("cccc4f7e-ea3a-426b-a5e3-ba694b84a614", $ids)){
-    echo "BEng!!!";
-} else{
-    echo "Not BEng!!!";
-}
+//echo "<br>------<br>";
+//if (in_array("cccc4f7e-ea3a-426b-a5e3-ba694b84a614", $ids)){
+//    echo "BEng!!!";
+//} else{
+//    echo "Not BEng!!!";
+//}
 // header("location: user.php");
+foreach($ids as $id){
+echo "$id <br>";
+}

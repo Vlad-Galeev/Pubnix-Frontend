55a0f2d0b5742179aa3402bcca08e753ec479709
Vlad <vlad.wpg@gmail.com>
2024-11-10 19:47:18 -0500

    Dev Commit spam

--- msCallback.php
+++ msCallback.php
@@ -6,7 +6,13 @@ require "vendor/autoload.php";
 use myPHPnotes\Microsoft\Auth;
 use myPHPnotes\Microsoft\Handlers\Session;
 use myPHPnotes\Microsoft\Models\User;
-
+echo Session::get("tenant_id");
+echo Session::get("client_id");
+echo Session::get("client_secret");
+echo Session::get("redirect_uri");
+echo Session::get("scopes");
+echo Session::get($_REQUEST['code']);
+echo Session::get("state");
 $microsoft = new Auth(Session::get("tenant_id"),Session::get("client_id"),  Session::get("client_secret"), Session::get("redirect_uri"), Session::get("scopes"));
 $tokens = $microsoft->getToken($_REQUEST['code'], Session::get("state"));
 

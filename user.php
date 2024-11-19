1865a8b6de3922739f50103f92fe2fe1b75c0b3f
Vlad <vlad.wpg@gmail.com>
2024-11-18 22:54:31 -0500

    Dev Commit spam

--- user.php
+++ user.php
@@ -6,6 +6,8 @@ use myPHPnotes\Microsoft\Auth;
 use myPHPnotes\Microsoft\Handlers\Session;
 use myPHPnotes\Microsoft\Models\User;
 // check cookies
+echo Session::get("tenant_id");
+
 if (is_null(Session::get("tenant_id"))) {
     // if not logged in
     // login logic
